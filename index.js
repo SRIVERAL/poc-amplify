864eedc2075d4f83507f1ae7a08f75d5e75dd663
Santiago Rivera Loaiza <santiago@catador.tech>
2023-10-26 23:27:38 +0000

    Add index.html v3

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const http = require('http');
+import { createServer } from 'http';
 
-const server = http.createServer((req, res) => {
+const server = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hola mundo\n');
